9554620051db9d222509c018686c58050ca877bb
Grant Willison <emailgdw@gmail.com>
2017-04-25 15:45:59 -0700

    Complete halfway through book. Continue to finish exercise

--- booklist.js
+++ booklist.js
@@ -1,7 +1,57 @@
-var Book = undefined; //Replace this with a book class
+// class Book {
+//   constructor(title, genre, author, read, readDate = undefined) {
+//     this.title =title;
+//     this.genre = genre;
+//     this.author = author;
+//     this.read = read;
+//     this.readDate = new Date();
+//   }
 
-var BookList = undefined; // replace this with a BookList class
+class Book {
+  constructor(title, genre, author, read, readDate) {
+    this.title =title;
+    this.genre = genre;
+    this.author = author;
+    this.read = read;
+    this.readDate;
 
+    if(this.read) {
+      this.readDate = new Date(Date.now())
+    };
+  }
+};
+
+class BookList {
+  constructor(){
+      this.readBooks = 0;
+      this.unreadBooks =0;
+      this.nextBook ={};
+      this.previousBook ={};
+      this.currentBook ={};
+      this.books =[];
+    }
+
+}; // replace this with a BookList class
+
+BookList.prototype.add = function (book) {
+    this.books.push(book);
+    if (book.read) {
+      this.readBooks++;
+    } else {
+      this.unreadBooks++;
+    }
+    if (this.currentBook.length < 1 ) {
+      this.currentBook.push(book);
+    } else if (this.nextBook.length < 1 && !this.read) {
+      this.nextBook.push(book);
+      //Object.keys(this.nextBook).length <= 1
+    }
+};
+class ClassName {
+  constructor() {
+
+  }
+}
 
 module.exports = {
   Book: Book,
--- cat.js
+++ cat.js
@@ -1,7 +1,56 @@
-var Cat = undefined; //replace this with a class
+//
+// class Cat {
+//   constructor(name, age) {
+//     this.name =name;
+//     this.age = age;
+//   }
+// }
+//es5
+let Cat = function() {
+  // name, age
+  // this.name =name;
+  // this.age = age;
+  this.tiredness = 0;
+  this.hunger = 0;
+  this.loneliness = 0;
+  this.happiness = 0;
 
 
 
+};
+//single fn def shared amoungst all instances
+Cat.prototype.feed = function(quantity = 1){
+      this.happiness += 1;
+      this.hunger -= quantity;
+  //return this.happiness, this.hunger;
+};
+
+Cat.prototype.status = function () {
+      //return `the happiness of the cat is ${this.happiness} while loneliness is ${this.loneliness}, fortunately tiredness is reported as ${this.tiredness}, and kitty is ${this.hunger} hunger`;
+        return `Cat status: tiredness = ${this.tiredness}, hunger = ${this.hunger}, loneliness = ${this.loneliness}, and happiness = ${this.happiness}.`
+};
+
+Cat.prototype.pet = function (minutes = 1) {
+   var fickle = Math.round(Math.random())
+   // BONUS   -----------------------------------commented out----------------------------
+    //if (fickle === 1) {
+      this.loneliness-= minutes;
+      this.happiness += (1.5 * minutes);
+    // } else {
+    //   console.log("YYOOOOwwwwLLLLL");
+    // }
+};
+Cat.prototype.sleep = function (hours = 1) {
+    this.tiredness -= hours;
+    this.happiness += hours;
+
+};
+// Cat should have a .status() method that returns it's status as a string,
+// including all state information, along with property names:
 module.exports = {
   Cat: Cat
 }
+
+// var joey = new Cat('joey', 5);
+// joey.feed(2, 2);
+// console.log(joey.tiredness, joey.hunger);
