e857a582ce3d2b7ae147e9e2838962ce24c40624
Grant Willison <emailgdw@gmail.com>
2017-05-24 20:02:51 -0700

    almost finished game and test on master

--- game.js
+++ game.js
@@ -1,14 +1,15 @@
 class Player {
   constructor(playerName) {
-    this.player = playerName;
+    this.name = playerName;
     this.wins = 0;
     this.losses = 0;
     this.roundsPlayed = 0;
     //this.move = undefined;
   }
-  move() {
-    this.odds = Math.random(Math.random());
-    console.log(this.odds);
+  hand() {
+    //randomize odds of picking any hand gesture
+    //this.odds = Math.random(Math.random()).toFixed(2);
+    // console.log(this.odds);
     if (this.odds >= .67) {
       this.move = 'rock';
       this.beats = 'scissors';
@@ -22,8 +23,9 @@ class Player {
       this.beats = 'paper';
     }
   }
+  //console.log(this.player.move);
   status(){
-    console.log(`${this.player} has ${this.wins} wins and ${this.losses} losses`);
+    //console.log(`${this.player} has ${this.wins} wins and ${this.losses} losses`);
     return `${this.player} has ${this.wins} wins and ${this.losses} losses`;
   }
 }
@@ -32,36 +34,43 @@ class Game {
   constructor(p1, p2) {
     this.player1 = p1;
     this.player2 = p2;
-
+    // console.log(this.player1, this.player2);
   }
+
   addRoundRecord(){
     this.player1.roundsPlayed++;
     this.player2.roundsPlayed++;
   }
   decideWinner() {
     if (this.player1.wins > this.player2.wins) {
-      console.log(`${this.player1} wins the game with ${this.player1.wins} wins!`);
+      // console.log(`${this.player1} wins the game with ${this.player1.wins} wins!`);
+      return this.player1.name + `wins the game with ${this.player1.wins} wins!`;
+    } else if (this.player1.wins == this.player2.wins) {
+        return 'its a tie game!';
+    }else if (this.player2.wins > this.player1.wins){
+      // console.log(`${this.player2} wins the game with ${this.player2.wins} wins!`);
+      return this.player2.name + `wins the game with ${this.player2.wins} wins!`
     } else {
-      console.log(`${this.player2} wins the game with ${this.player2.wins} wins!`);
+      return "something went wrong with your game if no tie or win is recorded, this isn\'t very fun";
     }
   }
   turn() {
-    if (this.player2.move  == this.player1.move ) {
-      console.log('tie game, no points awarded');
-      addRoundRecord();
+    if (this.player2.move === this.player1.move ) {
+      // console.log('tie game, no points awarded');
+      this.addRoundRecord();
     } else if (this.player2.beats === this.player1.move) {
       this.player2.wins++;
       this.player1.losses++;
-      addRoundRecord();
-      console.log(`${this.player2} wins`);
+      this.addRoundRecord();
+      // console.log(this.player2, `wins`);
     } else if (this.player1.beats = this.player2.move) {
       this.player1.wins++;
       this.player2.losses++;
-      console.log(`${this.player1} wins`);
-      addRoundRecord();
+      // console.log(this.player1, `wins`);
+      this.addRoundRecord();
     }
     if (this.player1.roundsPlayed > 2) {
-      decideWinner(this.player1, this.player2);
+      this.decideWinner(this.player1.name, this.player2.name);
     }
 
   }
@@ -69,14 +78,27 @@ class Game {
 }
 
 
+// let grant = new Player("gWilli");
+// let loser = new Player("rFitz");
+// grant.odds = .30;
+// loser.odds = .65;
+// let game2 =new Game(grant, loser);
+// game2.turn(grant.move(), loser.move());
+// console.log(game2);
 
 function bestOf3(player1, player2) {
   let grant = new Player(player1);
   let loser = new Player(player2);
-  let game1 =new Game(grant.move(), loser.move());
-  console.log(grant.status(), loser.status());
-  game1.turn();
-  console.log(game1);
+  var game = new Game(grant, loser);
+  for (var i = 1; i < 4; i++) {
+
+    console.log(grant);
+    game.turn(grant.hand(), loser.hand());
+    // console.log(grant.status(), loser.status());
+  }
+  //let game3 =new Game(grant, loser);
+ //game3.turn(grant.move(), loser.move());
+
 }
 
 console.log(bestOf3("gMoney", "ronan"));
--- test/game.test.js
+++ test/game.test.js
@@ -2,3 +2,70 @@ var expect = require('chai').expect;
 
 var Game = require("../game.js").Game;
 var Player = require("../game.js").Player;
+
+describe('Round of RoShamBo', function () {
+  var grant;
+  var loser;
+  var game1;
+  before(function () {
+    grant = new Person();
+    loser = new Person()
+    game1 = new Game();
+  });
+  it("should have wins, losses, number of rounds played and a name", function () {
+    expect(grant.losses).to.be.a('number');
+    expect(grant.wins).to.be.a('number');
+    expect(gant.roundsPlayed).to.be.a('number');
+    expect(grant.name)to.be.a("string");
+    expect('grant').to.have.ownProperty(wins);
+    expect('grant').to.have.ownProperty(losses);
+    expect('grant').to.have.ownProperty(roundsPlayed);
+    expect('grant').to.have.ownProperty(name);
+    expect('grant').to.have.ownProperty(move);
+
+  });
+
+  it("should play 3 rounds before the game is over", function () {
+    expect()
+  })
+  it("should be part of a class called Player", function () {
+    expect(grant).to.be.an.instanceOf(Player);
+    expect(loser).to.be.an.instanceOf(Player);
+
+  });
+  it('Should have players interact with a class called Game', function () {
+      expect(game1).to.be.an.instanceOf(Game);
+  });
+  it('Should have a function that changes the move the player is making', function () {
+    expect(hand).to.change(grant, 'move');
+  });
+  it('Should have a method to change the number of games played', function () {
+    expect(game1.addRoundRecord).to.change(grant, 'roundsPlayed');
+    expect(gmme1.turn).to.increase(grant, 'roundsPlayed');
+  });
+
+});
+
+describe('Game', function() {
+var game1, grant, loser;
+    before(function () {
+      grant = new Person();
+      loser = new Person()
+      game1 = new Game();
+    });
+    it("should have players and start each with zero games played", function () {
+      it("should have player1, player2, with undefined loser and winner", function() {
+        expect(game1.player1).to.be.a("object");
+        expect(game1.player2).to.be.a("object");
+        expect(game1.player1.roundsPlayed).to.equal(0);
+        expect(game1.player2.roundsPlayed).to.equal(0);
+});
+    })
+    it("Should have a player win when their hand beats the other", function () {
+      grant.odds = .70;
+      loser.odds = .29;
+      expect(game1.decideWinner).to.be
+    });
+
+
+}
